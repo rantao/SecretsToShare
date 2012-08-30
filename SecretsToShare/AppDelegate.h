f4c61e1a0c3318a92955839be22f4d257a8ffd3f
Ran Tao <ran@ephexi.com>
2012-08-30 11:14:26 -0700

    Initial Commit

---
+++ SecretsToShare/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SecretsToShare
+//
+//  Created by Ran Tao on 8.30.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
